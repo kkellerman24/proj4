98d21a62894ace79ed84d2ef67149233108b939c
kkellermanj97 <kkellerman97@gmail.com>
2019-05-04 14:43:49 -0500

    openmp jobs submitted
    Hope this works!!!!

--- openmp/sbatch_openmp.sh
+++ openmp/sbatch_openmp.sh
@@ -4,7 +4,7 @@
 #	Type(3): pthreads, openmp, mpi
 #	Cpu(10): 1,2,4,8,16,32,2x4, 4x4, 16x2, 2x16
 #	Problem size(4): 10k, 100k, 500k, 1M
-mem='32G'
+mem='8G'
 cons='elves'
 ptype='openmp'
 capptype='OPENMP'
@@ -22,7 +22,7 @@ do
     do
 #     for ((a = 0; a < 10; a++))
 #      do
-     sbatch --constraint=$cons --mem-per-cpu=3G  --mail-type=END --time=1:00:00 --mem=$mem --ntasks-per-node=$i --nodes=1 lcs_$ptype.sh "$capptype $cons CORES: $i" $j $k 
+     sbatch --constraint=$cons --mem-per-cpu=3G  --mail-type=END --time=4:00:00 --mem=$mem --ntasks-per-node=$i --nodes=1 lcs_$ptype.sh "$capptype $cons CORES: $i" $j $k 
 #      done 
    done
   done
