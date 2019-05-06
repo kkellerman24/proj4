15da1ec4652c59986c56c08171dbdc45518f2d64
cmccormick42 <47217759+cmccormick42@users.noreply.github.com>
2019-05-05 20:50:20 -0500

    Update lcs_mpi.c

--- mpi/lcs_mpi.c
+++ mpi/lcs_mpi.c
@@ -133,7 +133,7 @@ void *lcs_threading(void *id)
 	
 	for (i = startPos; (i < endPos) && (i+1<fileLines); i++)
 	{
-		//printf("%d-%d: %s\n", i, i+1, results[i]);
+		printf("%d-%d: %s\n", i, i+1, results[i]);
 	}
 	
 	if ((int)id < NUM_THREADS-1)
